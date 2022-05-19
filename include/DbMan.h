702282416adb45492b95961c9020d29c20325ac0
Joshua Boyd <jaboydfamily@gmail.com>
2022-05-19 08:10:50 -0400

    added backtester code

--- include/DbMan.h
+++ include/DbMan.h
@@ -14,14 +14,20 @@ class DbMan {
 		bool checkForQuoteTable(std::string ticker);
 		void createQuoteTable(std::string ticker);
 		void insertToQuoteTable(Quote quote, std::string ticker);
+		void getTableNames();
+		void getQuoteData();
 
 	private:
 		static int quoteTableCheckCallback(void *NotUsed, int argc, char **argv, char **azColName);
 		static int quoteTableCreationCallback(void *NotUsed, int argc, char **argv, char **azColName);
 		static int quoteTableInsertionCallback(void *NotUsed, int argc, char **argv, char **azColName);
+		static int getTableNamesCallback(void *NotUsed, int argc, char **argb, char **azColName);
+		static int quoteDataCallback(void *NotUsed, int argc, char **argb, char **azColName);
 
 		sqlite3 *db;
 		int rc;
 		char *zErrMsg = 0;
+		std::vector<Quote> quoteData;
+		std::vector<std::string> tableNamesData;
 
 };
---
+++ src/BackTester.cpp
@@ -0,0 +1,17 @@
+#include "BackTester.h"
+
+BackTester::BackTester() {
+	// constructor
+}
+
+BackTester::~BackTester() {
+	// deconstructor
+}
+
+void BackTester::setStartingCash(float amount) {
+	money = amount;
+}
+
+void BackTester::performBackTest(Strategy testStrat) {
+
+}
